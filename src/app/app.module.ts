c416761e0a38a5bc2f4e34f67a6b56e5824108ce
Kemon97 <kevin.montoya5146@uco.net.co>
2023-05-17 21:01:36 -0500

    Updating inventapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
-
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from "@angular/router";
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,17 +1,45 @@
 import { Component } from '@angular/core';
 
+interface Product{
+  id: number;
+  nombre: string;
+  marca: string;
+}
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
+
 export class ProductComponent {
-  nombre: string = 'Goku';
-  edad: Number = 25;
+  products: Product[] = [];
+  newProduct: Product = {id: 0, nombre: '', marca: ''}
+  productSelected: Product | null = null;
 
-  constructor() {};
+  productRegister(){
+    const newID = this.products.length + 1;
 
-  obtenerInfo(): string {
-    return `hola soy ${this.nombre} y tengo ${this.edad} años`;
+    this.newProduct.id = newID;
+    this.products.push(this.newProduct);
+
+    this.newProduct = {id: 0, nombre: '', marca: ''};
+  }
+
+  selectProduct(product: Product) {
+    this.productSelected = product;
+  }
+
+  modifyProduct() {
+    this.productSelected = null;
+  }
+
+  clearProduct(product: Product){
+    const index = this.products.findIndex(p => p.id === product.id);
+
+    if(index !== -1){
+      this.products.splice(index, 1);
+    }
   }
 }
+
