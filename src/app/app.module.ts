7d1a9381dbca1cd114e7ab5c34dbebea335ab586
Kemon97 <kevin.montoya5146@uco.net.co>
2023-06-14 16:02:58 -0500

    ....

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; // Importa FormsMod
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
-import { CookieService } from "ngx-cookie-service";
-
+import { CookieService } from 'ngx-cookie-service';
 
 import { AppComponent } from './app.component';
 import { InventoryComponent } from './inventory/inventory.component';
@@ -23,7 +23,7 @@ import { ClientComponent } from './client/client.component';
     RegisterComponent,
     LoginComponent,
     MainComponent,
-    ClientComponent
+    ClientComponent,
   ],
   imports: [
     BrowserModule,
@@ -31,8 +31,7 @@ import { ClientComponent } from './client/client.component';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    ToastrModule.forRoot()
-    
+    ToastrModule.forRoot(),
   ],
   providers: [CookieService],
   bootstrap: [AppComponent]
